d1b9ee7c6d90eb8af4c510de1d14bf86c7b2ad15
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-07-05 16:36:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ mytext = 'Your Text :)'
 
 language = 'en'
 
-myobj = gTTS(text = mytext, lang = language, slow = false)
+myobj = gTTS(text = mytext, lang = language, slow = False)
 
 myobj.save("file_name.mp3")
 os.system("mp3 file_name.mp3")
